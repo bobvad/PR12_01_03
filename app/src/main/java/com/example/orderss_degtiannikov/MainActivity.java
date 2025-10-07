ec44abcc5df36e1f1a77b626963e6edac22d7920
Djfjfj2006@outlook.com <Djfjfj2006@outlook.com>
2025-10-08 01:44:02 +0500

    Разработанная функция AlertDialog.Дегтянников

--- app/src/main/java/com/example/orderss_degtiannikov/MainActivity.java
+++ app/src/main/java/com/example/orderss_degtiannikov/MainActivity.java
@@ -1,12 +1,12 @@
 package com.example.orderss_degtiannikov;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.core.app.TaskStackBuilder;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,10 +15,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+    }
+    public void AlertDialog(String title,String message)
+    {
+        AlertDialog.Builder Builder = new AlertDialog.Builder(MainActivity.this);
+        Builder.setTitle(title)
+                .setMessage(message)
+                .setCancelable(false)
+                .setNegativeButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int i) {
+                        dialog.cancel();
+                    }
+                });
+        AlertDialog Alert = Builder.create();
+        Alert.show();
     }
 }
